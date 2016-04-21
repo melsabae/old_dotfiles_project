c655cd9643d78489001576fe7d1ea7318dfa27f8
Mohammad El-Sabae <mohammad.elsabae@gmail.com>
2016-04-21 14:18:16 -0500

    lots of testing needs to be done, especially for path resolutions

--- scripts/doit.sh
+++ scripts/doit.sh
@@ -2,6 +2,7 @@
 
 #laziness++ grab the parent dir of scripts
 backupdir=$(dirname $(dirname $(realpath $0)))
+scriptdir="$backupdir/scripts"
 oldfiledir="$backupdir/old_dotfiles"
 
 if [ ! -d $oldfiledir ]
@@ -9,90 +10,72 @@ then
 	mkdir $oldfiledir
 fi
 
-if [ -f ~/"notarealfileihope" ]
-then
-	echo $backupdir/$backup
-	echo "Creating symlink: ~/$backup -> $backupdir/$backup"
 
-	for backup in $(ls -A $backupdir)
-	do
-		#skip these things
-		if [ $backup == ".git" -o $backup == ".gitignore" -o $backup == "misc" -o $backup == "scripts" \
-			-o $backup == $oldfiledir ]
+for backup in $(ls -A $backupdir)
+do
+	#skip these things
+	if [ $backup == ".git" -o $backup == ".gitignore" -o $backup == "misc" -o $backup == "scripts" -o $backup == $oldfiledir ]
+	then
+		continue
+	fi
+
+	# if there is no file/dir with $backup's name in ~, and the current $backup is a file, then symlink it
+	if [ ! -e ~/$backup ] && [ -f $backupdir/$backup ]
+	then
+		echo "Creating symlink: ~/$backup -> $backupdir/$backup"
+		ln -s $backupdir/$backup ~/$backup
+		continue
+	fi
+
+
+	if [ -f $backupdir/$backup ] # if the backup is a file
+	then
+		if [ ! -L ~/$backup ] # file exists but is not a symlink
 		then
-			continue
-		fi
-
-
-		if [ -f $backupdir/$backup ] # if the backup is a file
-		then
-			if [ ! -f ~/$backup ] # if there is no file or symlink to this backup
+			read -p "Replace file ~/$backup with symlink to $backupdir/$backup ? " answer
+			if [ answer == "y" -o answer == "Y" ]
 			then
+				mv ~/$backup $oldfiledir/$backup
 				ln -s $backupdir/$backup ~/$backup
-			else
-				if [ ! -L ~/$backup ] # file exists but is not a symlink
+			fi
+		else
+			if [ $(readlink ~/$backup) != $(realpath $backupdir/$backup) ] #TODO replace string comparisons with file path comparisons
+			then
+				read -p "Replace symlink ~/$backup with symlink to $backupdir/$backup ? " answer
+				if [ answer == "y" -o answer == "Y" ]
 				then
-					read -p "Replace file ~/$backup with symlink to $backupdir/$backup ? " answer
-					if [ answer == "y" -o answer == "Y" ]
-					then
-						mv ~/$backup $oldfiledir/$backup
-						ln -s $backupdir/$backup ~/$backup
-					fi
-				else
-					if [ $(readlink ~/$backup) != $backupdir/$backup ] #if the symlink is not the same target
-					then
-						read -p "Replace symlink ~/$backup with symlink to $backupdir/$backup ? " answer
-						if [ answer == "y" -o answer == "Y" ]
-						then
-							rm ~/$backup
-							ln -s $backupdir/$backup ~/$backup
-						fi
-					fi
+					rm ~/$backup
+					ln -s $backupdir/$backup ~/$backup
 				fi
 			fi
 		fi
+	fi
 
-		if [ -d $backup ]
-		then
-			continue
-		fi
-
-
-		#if [ -f $backup ] #backup is a file
-		#then
-		#if [ ! -f ~/$backup ] #if backup is not reached from home dir
-		#then
-		#echo "Creating symlink: ~/$backup -> $(pwd)/$backup"
-		#ln -s $(pwd)/$backup ~/$backup
-		#else
-		#if [ ! -L ~/$backup ]
-		#then
-		#read -p "Replace file ~/$backup with symlink to $(pwd)/$backup ? " answer
-		#if [ answer == "y" -o answer == "Y" ]
-		#then
-		#mv ~/$backup $(dirname
-		#ln -s $(pwd)/$rcfile/$rcdir ~/$rcfile/$rcdir
-		#fi
-		#fi
-		#fi
-		#fi
-
-		#if [ -d $backup ] #backup is a directory
-		#then
-		#continue
-		#fi
-
-		echo
-	done
-else
-	for backup in $(ls -A $backupdir)
-	do
-		echo $backupdir/$backup
-	done
-fi
-
-		echo $oldfiledir
+	if [ -d $backup ] # if the backup is its own dir of backups
+	then
+		for $rcdir in $(ls -A $backupdir/$backup)
+		do
+			if [ ! -e ~/$backup/$rcdir ] #if no dir or link exists, make it
+			then
+				echo "Creating symlink: ~/$backup/$rcdir -> $backupdir/$backup/$rcdir"
+				ln -s $backupdir/$backup/$rcdir ~/$backup/$rcdir
+				continue
+			else
+				if [ ! -L ~/$backup/$rcdir ] # if the dir exists but is not a link, prompt and make
+				then
+					#TODO
+					continue
+				elif [ $(readlink ~/$backup/$rcdir) != $(realpath $backupdir/$backup/$rcdir) ] #if the symlink is not pointing to the repo
+				then
+					#TODO
+					continue
+				fi
+			fi
+		done
+		continue
+	fi
+done
 
 # load and merge .Xresources file
-#$(dirname $0)/xres.sh #TODO: resolve path correctly
+$scriptdir/xres.sh
 
