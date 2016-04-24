bb61a80a082b0d17318635cc054513fe904768cd
Mohammad El-Sabae <mohammad.elsabae@gmail.com>
2016-04-24 00:21:56 -0500

    forgot about the rc

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -82,7 +82,7 @@ tags = {}
 for s = 1, screen.count() do
 	-- Each screen has its own tag table.
 	--tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[1])
-	tags[s] = awful.tag({ "p ", "s ", "t "}, s, layouts[1])
+	tags[s] = awful.tag({ "1", "2", "3"}, s, layouts[1])
 end
 -- }}}
 
